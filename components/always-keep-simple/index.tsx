87fec6d8a85bdc5732f2e10f5ad3c13384e96dde
Pitech-Project <karan.shah@pitechniques.com>
2025-05-08 11:23:40 +0530

    img animation - always keep simple section

--- components/always-keep-simple/index.tsx
+++ components/always-keep-simple/index.tsx
@@ -2,7 +2,7 @@
 
 import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { BusinessOffice, BusinessOfficeImg, CaseStudiesBannerImg, FullStop,  SubTypography } from '@/app/styles/MUI/common.styled';
-// import CareerImgAnimation from '../career-img-animation';
+import CareerImgAnimation from '../career-img-animation';
 
 type ThemeType = {
     darkTheme: boolean;
@@ -18,7 +18,7 @@ export const AlwaysKeepSimple = ({darkTheme} : ThemeType) => {
                 <Typography variant='body_1' margin={isSmallDesktopView ? '0 auto' : 'auto'} display={'block'} width={isTabletView ? 'auto' : 'max-content'} color={darkTheme ? 'custom.white2' : 'custom.black'}>Always keep it <br></br> simple <FullStop /></Typography>
                 <SubTypography variant='Caption' color={darkTheme ? 'custom.black1' : 'custom.black1'} display={'block'}>Strong team with a passion for technology</SubTypography>
             </Box>
-            {/* {darkTheme ? (<CareerImgAnimation />) : (<CaseStudiesBannerImg className='alwaysKeepSimple' />)} */}
+            {darkTheme ? (<CareerImgAnimation />) : (<CaseStudiesBannerImg className='alwaysKeepSimple' />)}
         </BusinessOffice>
     );
 };
