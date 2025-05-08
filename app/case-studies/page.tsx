362dae0be6153a13c675980d5f3098da95842bf2
Pitech-Project <karan.shah@pitechniques.com>
2025-05-08 16:46:16 +0530

    header

--- app/case-studies/page.tsx
+++ app/case-studies/page.tsx
@@ -6,8 +6,17 @@ import { ShipManagement } from "@/components/ship-management";
 import { ShipManagement2 } from "@/components/ship-management2";
 import "../global/globals.css";
 import { ThemeProvider } from "@mui/material";
+import { useEffect } from "react";
 
 export default function Page() {
+
+	useEffect(() => {
+		document.body.classList.add("case-studies");
+
+		return () => {
+			document.body.classList.remove("case-studies");
+		};
+	}, []);
 
   return (
     <>
