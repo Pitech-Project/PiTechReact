77f36f1c48328ca6a31a6403dd9841b7f56ff1de
Pitech-Project <karan.shah@pitechniques.com>
2025-05-09 18:08:31 +0530

    page

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,24 @@
 import { Box, ThemeProvider } from "@mui/material";
 import { lightTheme } from "./styles/MUI/theme";
 import "./global/globals.css";
-import TeamBusinessOffice from "@/components/team-business-office";
 import Count from "@/components/count";
 import { Services } from "@/components/services";
 import Testimonial from "@/components/testimonials";
 import SmoothTextSlider from "@/components/top-companies";
 import WhoWeAre from "@/components/who-we-are";
 import OurWorkCarousel from "@/components/our-work-carousel";
+import HomeBanner from "@/components/home-banner";
 
-export default function Home() {
+export default function Home(homeSlider: boolean) {
   return (
     <ThemeProvider theme={lightTheme}>
       <Box bgcolor={'custom.bgcolor'}>
-        <TeamBusinessOffice title={"Commitment"} afterbreaktitle={"to customised solutions"} subtitle={"Inside of every problem lies an opportunity"} buttondisplay={true} />
+        <HomeBanner />
         <Count />
         <WhoWeAre title={"who we are "} buttondisplay={true} aboutus={false}/>
         <Services />
         <OurWorkCarousel />
-        <SmoothTextSlider />
+        <SmoothTextSlider homeSlider={homeSlider}/>
         <Testimonial />
       </Box>
     </ThemeProvider>
