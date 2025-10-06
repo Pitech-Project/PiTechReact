a411b05926dfdbbf08ffd56232967bfd39a7bbda
Pitech-Project <karan.shah@pitechniques.com>
2025-10-06 15:58:58 +0530

    Add files via upload

--- types/custom-elements.d.ts
+++ types/custom-elements.d.ts
@@ -1,5 +1,23 @@
 declare namespace JSX {
   interface IntrinsicElements {
-    'dotlottie-player': any;
+    "dotlottie-player": DotLottiePlayerAttributes;
   }
+}
+
+// The custom DOM element instance
+interface DotLottiePlayerElement extends HTMLElement {
+  play?: () => void;
+  pause?: () => void;
+}
+
+// JSX attributes allowed on <dotlottie-player />
+interface DotLottiePlayerAttributes
+  extends React.HTMLAttributes<DotLottiePlayerElement> {
+  ref?: React.Ref<DotLottiePlayerElement>; // ✅ Add this line
+  src?: string;
+  background?: string;
+  speed?: string;
+  loop?: boolean;
+  autoplay?: boolean;
+  style?: React.CSSProperties;
 }
