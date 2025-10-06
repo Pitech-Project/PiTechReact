c38be08d4fd7ffd32d5cba9ba99d7e0d29db6f01
Pitech-Project <karan.shah@pitechniques.com>
2025-10-06 15:55:53 +0530

    Add files via upload

--- lib/constanst.ts
+++ lib/constanst.ts
@@ -55,12 +55,30 @@ export const careerFormFields: FieldConfig[] = [
 ];
 
 export const SERVICES = {
-  SERVICES1: "Tailored and templated – apps that make an impact",
+  SERVICES1: "Tailored and templated apps that make an impact",
+  SERVICES7: "Harnessing Generative AI for Real-World Impact",
   SERVICES2: "Strategic intelligence, real-time decisions",
-  SERVICES3: "Infrastructure that moves at the speed of business",
-  SERVICES4: "Sleek interfaces compliment effortless journeys ",
-  SERVICES5: "Strategic mobile innovation built with precision",
-  SERVICES6: "Web experiences that click",
+  SERVICES3: "Sleek interfaces compliment effortless journeys ",
+  SERVICES4: "Strategic mobile innovation built with precision",
+  SERVICES5: "Web experiences that click",
+  SERVICES6: "Infrastructure that moves at the speed of business",
+  SERVICES8: "Connecting systems, creating seamless workflows.",
+};
+
+export const LOTTIEANIMATION = {
+  LottiePlayer1: `https://lottie.host/e099a1fa-cc41-4a4c-b468-45d1efaabd08/ZI74c8HxKR.lottie`,
+  LottiePlayer7: `https://lottie.host/e426f7e1-e5f9-453d-b638-c4271bd55b19/OUKW82oEhF.lottie`,
+  LottiePlayer2: `https://lottie.host/5ddf6e19-2253-4413-a68a-4395b41fafd9/PyspVcKRLz.lottie`,
+  LottiePlayer3:
+    "https://lottie.host/8b9c39bd-c58f-4cf9-b147-492435685393/ScoJHZOyj9.lottie",
+  LottiePlayer4:
+    "https://lottie.host/9588ccb3-f1b5-448d-8940-605f112cbd84/ACvLh4sJQ4.lottie",
+  LottiePlayer5:
+    "https://lottie.host/357aebd0-19ba-45bd-9058-dd4b9a2f8297/hr0mQTHeMm.lottie",
+  LottiePlayer6:
+    "https://lottie.host/9274627e-7752-49fb-b054-f4d91750bf3e/sPhBzDgj8F.lottie",
+  LottiePlayer8:
+    "https://lottie.host/41aca960-62bb-4e93-b976-bbff74c44864/pDzfVTtitx.lottie",
 };
 
 export const caseStudy = [
